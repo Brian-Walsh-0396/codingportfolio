a377690d0af77f35d7a57f161e422fc9478acd07
Brian-Walsh-0396 <110205346+Brian-Walsh-0396@users.noreply.github.com>
2023-03-13 19:32:34 -0400

    New code added
    Cleaned up areas, data now exports to a txt file.

--- Personal Projects/Python/5e Quick Builder/Quickbuilder.py
+++ Personal Projects/Python/5e Quick Builder/Quickbuilder.py
@@ -2,6 +2,7 @@ import random
 import os
 import time
 
+start_time = time.time()
 
 def d20():
     total = 0
@@ -15,14 +16,14 @@ player_name = (input("What is your name? "))
 
 # Arrays
 
-#classes
+# classes
 classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk',
            'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
 
-#races
+# races
 races = ['Dwarf', 'Elf', 'Halfling', 'Human', 'Gnome', 'Half-Elf',
          'Half-Orc', 'Tiefling']
-#Abilities
+# Abilities
 
 abilities = {
     "barbarian": ["Strength", "Constitution"],
@@ -51,11 +52,11 @@ class_choice = int(input("Enter the number of your chosen class: "))
 
 # Verify that the user has entered a valid number and print their selection
 if class_choice in range(1, len(classes)+1):
-   print(f"You have chosen the {classes[class_choice-1]} class. The most important abilities are {', '.join(abilities[classes[class_choice-1].lower()])}")
+   print(
+       f"You have chosen the {classes[class_choice-1]} class. The most important abilities are {', '.join(abilities[classes[class_choice-1].lower()])}")
 else:
     print("Invalid input. Please try again and enter the number of your chosen class.")
 # Ask for player race
-
 
 
 print(f"Please select a D&D Race from the SRD, {player_name}: ")
@@ -75,4 +76,25 @@ else:
 # Ask for character name
 character_name = (input("What is your hero's name? "))
 
-print(f"Greetings mighty hero, {character_name} the {races[race_choice-1]} {classes[class_choice-1]}! They will be played by {player_name}.")
+print(
+    f"Greetings mighty hero, {character_name} the {races[race_choice-1]} {classes[class_choice-1]}! They will be played by {player_name}.")
+
+#exporting the data
+file_path = "C:/Users/Brian/Documents/Software Development Portfolio/Personal Projects/Python/5e Quick Builder"
+file_name = character_name
+file_ext = ".txt"
+file_path = os.path.join(
+    os.getcwd(), f"{file_name}{file_ext}")
+
+with open(file_path, "w") as file:
+    file.write(f"Player:{player_name}\n")
+    file.write(f"Race:{races[race_choice-1]}\n")
+    file.write(f"Class:{classes[class_choice-1]}\n")
+    file.write(f"***stats go here***")
+
+os.startfile(file_path)
+
+# next steps
+# add stat rolling
+# print to correct location
+# add formating
